7776bf1f5b0edf19e1b33c0276893abb1285071e
paco39 <pako03.fm@gmail.com>
2016-02-24 15:46:46 -0600

    Update Agenda.java

--- src/agenda/Agenda.java
+++ src/agenda/Agenda.java
@@ -10,50 +10,88 @@ import java.util.ArrayList;
 public class Agenda {
     ArrayList<Contacto> contactos;
     Agenda(){
-       
+       contactos=new ArrayList<Contacto>();         //Se crea la lista de contactos
     }
     public void registrarContacto(){
+        Scanner teclado = new Scanner(System.in);
+        String nombreContacto, fecha_nacimientoContacto,ocupacionContacto,direccionContacto;
+        
+        Contacto contacto= new Contacto();          //Se crea un objeto de tipo Contacto
+                                                    //Para almacenarlo en la lista
+        System.out.println("Nombre: ");
+        nombreContacto= teclado.nextLine();
+        contacto.setNombre(nombreContacto);
+        
+        System.out.println("Fecha de nacimiento: ");
+        fecha_nacimientoContacto= teclado.nextLine();
+        contacto.setFechaNacimiento(fecha_nacimientoContacto);  //Se leen los datos del Contacto
+        
+        System.out.println("Ocupacion: ");
+        ocupacionContacto= teclado.nextLine();
+        contacto.setOcupacion(ocupacionContacto);
+        
+        System.out.println("Direccion: ");
+        direccionContacto= teclado.nextLine();
+        contacto.setDireccion(direccionContacto);
+        
+        contacto.registrarTelefonos();                          //Se piden los telefonos
+        contacto.registrarRedesSociales();                      //Y redes sociales
+        
+        contactos.add(contacto); 
        
     }
     public void eliminarContacto(){
        
     }
     public void editarContacto(){
-       
+    }
+    public int tamanoLista(){                                   //Función que regresa el numero de 
+        int x=0;                                                //contactos guardados en la agenda
+        x=contactos.size();
+        return x;
+    }
+    public void mostrarContactos(){                             //Funcion que imprime los datos de cada contacto
+        for(int i=0;i<tamanoLista();i++){
+            System.out.println("-------------------------------------");
+            System.out.println("Contacto "+(i+1));
+            System.out.println("Nombre: "+contactos.get(i).getNombre());
+            System.out.println("Direccion: "+contactos.get(i).getDireccion());
+            System.out.println("Fecha de Nacimiento: "+contactos.get(i).getFechaNacimiento());
+            System.out.println("Ocupacion: "+contactos.get(i).getOcupacion());
+            contactos.get(i).mostrarTelefonos();
+            contactos.get(i).mostrarRedesSociales();
+        }
     }
     public static void main(String[] args) {
         Agenda miAgenda;
+        boolean boleano=true;
+        int x;
+        
         miAgenda= new Agenda();
         Scanner in = new Scanner(System.in);
-        Contacto miContacto;
-        miContacto= new Contacto();
-        String nombreContacto, fecha_nacimientoContacto,ocupacionContacto,direccionContacto,paginaWebContacto;
-       
-        System.out.println("ingresa nombre contacto");
-        nombreContacto  = in.next();
-        miContacto.setNombre(nombreContacto);
-       
-        System.out.println("ingresa la fecha de nacimiento del contacto (dd/mm/aaaa");
-        fecha_nacimientoContacto = in.next();
-        miContacto.setFechaNacimiento(fecha_nacimientoContacto);
-       
-        System.out.println("ingresa la ocupacion del contacto");
-        ocupacionContacto = in.next();
-        miContacto.setOcupacion(ocupacionContacto);
-       
-        System.out.println("ingresa la direccion del contacto");
-        direccionContacto = in.next();
-        miContacto.setDireccion(direccionContacto);
-       
-        System.out.println("ingresa la pagina web del contacto");
-        paginaWebContacto = in.next();
-        miContacto.setPaginaWeb(paginaWebContacto);
-       
-       
-       
-       
-       
-       
+        while(boleano==true){
+        System.out.println("1.- Agreagar contacto");
+        System.out.println("2.- Eliminar contacto");
+        System.out.println("3.- Mostrar contactos");
+            System.out.println("4.-Salir");
+        x=in.nextInt();
+        switch(x){
+            case 1:
+                miAgenda.registrarContacto();
+                break;
+            case 2:
+                break;
+            case 3:
+                miAgenda.mostrarContactos();
+                break;
+            case 4:
+                boleano=false;
+                break;
+            default:
+                System.out.println("Ingrese una opcion valida");
+                break;
+        }
+        }       
     }
-   
+
 }
